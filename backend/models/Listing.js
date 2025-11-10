c435cd485e37138ddb3f4dfd6e6927bc6ec3723e
Vikash Dhakar <mrvikashkirar478@gmail.com>
2025-11-10 16:45:50 +0530

    enhance collection

--- backend/models/Listing.js
+++ backend/models/Listing.js
@@ -7,11 +7,22 @@ const ImageSchema = new mongoose.Schema({
 }, { _id: false });
 
 const listingSchema = new mongoose.Schema({
+  main_category: { type: String },
   title: { type: String, required: true },
+  average_rating: { type: Number },
+  rating_number: { type: Number },
+  features: { type: [String] },
   description: { type: String },
-  price: { type: Number, required: true },
-  images: [ImageSchema],
-  createdAt: { type: Date, default: Date.now }
+  price: { type: Number },
+  images: { type: [String] },
+  videos: { type: [String] },
+  store: { type: String },
+  categories: { type: [String] },
+  details: { type: mongoose.Schema.Types.Mixed },
+  parent_asin: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  faiss_vector_id: { type: Number, default: null },
+  embedding_created_at: { type: Date, default: null }
 });
 
 module.exports = mongoose.model('Listing', listingSchema);
